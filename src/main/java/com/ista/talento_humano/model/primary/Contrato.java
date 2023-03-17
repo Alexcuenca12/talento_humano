4987873d027a045014d1d3cb88984dff63177b80
Alexis <mateosiavig@gmail.com>
2023-03-17 15:49:34 -0500

    Cambios

--- src/main/java/com/ista/talento_humano/model/primary/Contrato.java
+++ src/main/java/com/ista/talento_humano/model/primary/Contrato.java
@@ -1,4 +1,38 @@
 package com.ista.talento_humano.model.primary;
 
-public class Contrato {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "contrato")
+public class Contrato implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_contrato;
+
+    private Date fechaInicio;
+
+    private Date fechaFin;
+
+    private int anioDuracion;
+
+    private int horas;
+
+    private String cargo;
+
+    private String salario;
+
+
 }
