f6971aaab6f555d9549d7523cb4f5bad6b29c865
Alexis <mateosiavig@gmail.com>
2023-03-17 15:00:26 -0500

    CambiosPOM

--- src/main/java/com/ista/talento_humano/model/primary/Horario.java
+++ src/main/java/com/ista/talento_humano/model/primary/Horario.java
@@ -1,4 +1,18 @@
 package com.ista.talento_humano.model.primary;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "horario")
 public class Horario {
 }
